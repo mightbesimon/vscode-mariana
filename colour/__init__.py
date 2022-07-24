eb417149b90613a56771edbcf1ec780aa12a3028
mightbesimon <60593422+mightbesimon@users.noreply.github.com>
2022-07-24 17:27:14 +1200

    make colour/ into a module

---
+++ colour/__init__.py
@@ -0,0 +1,9 @@
+'''	Copyright © 2022 mightbesimon.com
+	All rights reserved.
+
+	Material belonging to others may have been
+	used under Creative Commons Licence or with
+	explicit or implicit permission.
+'''
+
+from .colour import Colour, hsla, rgba
